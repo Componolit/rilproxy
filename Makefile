a654c8f546e4524f6c684ba366932ed84c78dbe1
Alexander Senier <senier@componolit.com>
2017-11-24 09:37:32 +0100

    Add scripts for deployment on Android x86

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
+ISO_FILES=\
+	obj/local/x86_64/rilproxy_server \
+	scripts/install.sh \
+	scripts/rilproxy.rc \
+	scripts/rilproxy.sh \
+
 all::
 	ndk-build
-	genisoimage -JR -o deploy.iso obj/local/x86_64/*rilproxy*
+	genisoimage -JR -o deploy.iso ${ISO_FILES}
 	adb push obj/local/arm64-v8a/*rilproxy* /data/local/tmp/
 
 clean:
-	rm -rf obj libs
+	rm -rf obj libs deploy.iso
---
+++ scripts/install.sh
@@ -0,0 +1,27 @@
+#!/system/bin/sh
+
+set -eu
+
+SOURCE_DIR=/mnt/media_rw/CDROM
+
+echo "Mounting /system rw"
+mount -o rw,remount /system
+
+echo "Copying files"
+cp -v ${SOURCE_DIR}/rilproxy_server /system/bin/rilproxy_server
+cp -v ${SOURCE_DIR}/rilproxy.sh /system/bin/rilproxy.sh
+cp -v ${SOURCE_DIR}/rilproxy.rc /system/etc/init/rilproxy.rc
+
+echo "Fixing permissions"
+chmod 755 /system/bin/rilproxy.sh
+chmod 644 /system/etc/init/rilproxy.rc
+
+echo "Backing up RIL rc file"
+
+if [ -e "/system/etc/init/rild.rc" ];
+then
+    mv /system/etc/init/rild.rc /system/etc/init/rild.rc.bak
+fi
+
+# Set noril
+sed -i -e 's/ro.radio.noril yes/ro.radio.noril no/' /system/etc/init.sh
