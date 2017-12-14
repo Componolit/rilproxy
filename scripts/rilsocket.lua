c0bb4b8f36e64952d3ddee8663aeabe664d7e2ba
Alexander Senier <senier@componolit.com>
2017-12-14 22:55:26 +0100

    Reassemble RILd packets

--- scripts/rilsocket.lua
+++ scripts/rilsocket.lua
@@ -7,67 +7,71 @@ rilproxy.fields.length  = ProtoField.uint32('rilproxy.length', 'Length', base.DE
 rilproxy.fields.content = ProtoField.string('rilproxy.content', 'Content', base.HEX)
 
 function rilproxy.init()
-    pktState = {}
+    cache = ByteArray.new()
     bytesMissing = 0
 end
 
 function rilproxy.dissector(buffer, info, tree)
 
-    if buffer:len() < 4 then return end
+    -- Follow-up to a message where length header indicates
+    -- more bytes than available in the message.
+    if bytesMissing > 0
+    then
+
+        if buffer:len() > bytesMissing
+        then
+            print("Follow-up message longer (" .. buffer:len() .. ") than missing bytes (" .. bytesMissing .. "), ignoring")
+            bytesMissing = 0
+            cache = ByteArray.new()
+            return
+        end
+
+        cache:append(buffer(0):bytes())
+        bytesMissing = bytesMissing - buffer:len()
+
+        -- Still fragments missing, wait for next packet
+        if bytesMissing > 0
+        then
+            return
+        end
+
+        buffer = ByteArray.tvb(cache, "Packet")
+        cache = nil
+    end
 
     local buffer_len = buffer:len()
 
-    if bytesMissing > 0
-    then
-        -- FIXME: length must be <= bytesMissing
-        cache:append(buffer(0):bytes())
-        bytesMissing = bytesMissing - buffer_len
-        buffer = ByteArray.tvb(cache, "Packet")
+    -- Message must be at least 4 bytes
+    if buffer_len < 4 then
+        print("Dropping short buffer of len " .. buffer_len)
         return
     end
 
     local header_len = buffer(0,4):uint()
 
-    print("header_len=" .. header_len .. " buffer_len=" .. buffer_len)
-    if buffer_len < header_len - 4
+    if header_len > 1492
     then
-        print("Short buffer of length " .. buffer_len .. " (header len " .. header_len .. ")")
-        bytesMissing = header_len - buffer_len - 4
-        cache:append(buffer(0):bytes())
+        print("Skipping long buffer of length " .. header_len)
+        bytesMissing = 0
+        cache = ByteArray.new()
         return
     end
 
+    if buffer_len <= (header_len - 4)
+    then
+        print("Short buffer of length " .. buffer_len .. " (header len " .. header_len .. ")")
+        bytesMissing = header_len - buffer_len + 4
+        cache:append(buffer(0):bytes())
+        buffer = nil
+        return
+    end
 
-    local state = pktState[info.number]
-
-    --if state ~= nil
-    --then
-    --    -- Packet has already been processed previously
-    --    if state.complete == true
-    --    then
-    --        info.info = "Command [complete]"
-    --        buffer = ByteArray.tvb(state.buffer, "Complete command")
-    --    else
-    --        info.info = "Command [incomplete]"
-    --        return
-    --    end
-    --else
-    --    -- Packet is handled for the first time
-    --    state = {}
-
-    --    if cache == nil
-    --    then
-    --        cache = buffer(0):bytes()
-    --    else
-    --        cache:append(buffer(0):bytes())
-    --        -- New tvb for packet
-    --        buffer = ByteArray.tvb(cache, "Command")
-    --    end
-    --end
+    cache = ByteArray.new()
+    bytesMissing = 0
 
     local t = tree:add(rilproxy, buffer, "RIL Proxy")
-    t:add(rilproxy.fields.length, length)
-    t:add(rilproxy.fields.content, buffer(4, len))
+    t:add(rilproxy.fields.length, header_len)
+    t:add(rilproxy.fields.content, buffer(4, header_len))
 end
 
 local udp_port_table = DissectorTable.get("udp.port")
